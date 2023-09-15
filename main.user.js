c29bfbdf77fa0b4c3f460ea2735574323bb9ae83
gui-ying233 <zhuyufei20050307@gmail.com>
2023-09-14 22:36:56 -0400

    差异页面增加前后页面，等

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         灰机wiki查看版本历史
 // @namespace    https://github.com/gui-ying233/huijiHistory
-// @version      2.2.0
+// @version      2.3.0
 // @description  以另一种方式查看灰机wiki版本历史（绕过权限错误）。
 // @author       鬼影233, Honoka55
 // @match        *.huijiwiki.com/*
@@ -12,7 +12,7 @@
 
 (function () {
 	"use strict";
-	(async function () {
+	(async () => {
 		await new Promise(resolve => {
 			const intervId = setInterval(() => {
 				if (
@@ -125,6 +125,10 @@
 										.appendChild(li);
 								}
 							);
+							document.getElementById(
+								"mw-history-compare"
+							).innerHTML +=
+								'<div><input class="historysubmit mw-history-compareselectedversions-button" title="查看该页面两个选定的版本之间的差异。[ctrl-option-v]" accesskey="v" type="submit" value="对比选择的版本"></div>';
 						}
 					});
 				};
@@ -144,7 +148,6 @@
 						pageName
 					);
 					const api = new mw.Api();
-					// @todo: prev & next
 					if (searchParams.get("diff")) {
 						mw.util.addCSS(
 							`.diff{border:0;border-spacing:4px;margin:0;width:100%;table-layout:fixed}.diff td{padding:.33em .5em}.diff td.diff-marker{padding:.25em}.diff col.diff-marker{width:2%}.diff .diff-content{width:48%}.diff td div{word-wrap:break-word}.diff-title{vertical-align:top}.diff-multi,.diff-notice,.diff-ntitle,.diff-otitle{text-align:center}.diff-lineno{font-weight:700}td.diff-marker{text-align:right;font-weight:700;font-size:1.25em;line-height:1.2}.diff-addedline,.diff-context,.diff-deletedline{font-size:88%;line-height:1.6;vertical-align:top;white-space:-moz-pre-wrap;white-space:pre-wrap;border-style:solid;border-width:1px 1px 1px 4px;border-radius:.33em}.diff-addedline{border-color:#a3d3ff}.diff-deletedline{border-color:#ffe49c}.diff-context{background:#f8f9fa;border-color:#eaecf0;color:#222}.diffchange{font-weight:700;text-decoration:none}.diff-addedline .diffchange,.diff-deletedline .diffchange{border-radius:.33em;padding:.25em 0}.diff-addedline .diffchange{background:#d8ecff}.diff-deletedline .diffchange{background:#feeec8}.diff,.diff-currentversion-title{direction:ltr;unicode-bidi:embed}.diff-contentalign-right td{direction:rtl;unicode-bidi:embed}.diff-contentalign-left td{direction:ltr;unicode-bidi:embed}.diff-lineno,.diff-multi,.diff-ntitle,.diff-otitle{direction:ltr!important;unicode-bidi:embed}.mw-diff-movedpara-left,.mw-diff-movedpara-left:active,.mw-diff-movedpara-left:visited,.mw-diff-movedpara-right,.mw-diff-movedpara-right:active,.mw-diff-movedpara-right:visited{display:block;color:transparent}.mw-diff-movedpara-left:hover,.mw-diff-movedpara-right:hover{text-decoration:none;color:transparent}.mw-diff-movedpara-left:after,.mw-diff-movedpara-right:after{display:block;color:#222;margin-top:-1.25em}.mw-diff-movedpara-left:after,.rtl .mw-diff-movedpara-right:after{content:"↪"}.mw-diff-movedpara-right:after,.rtl .mw-diff-movedpara-left:after{content:"↩"}@media print{td.diff-addedline .diffchange,td.diff-context,td.diff-deletedline .diffchange{background-color:transparent}td.diff-addedline .diffchange{text-decoration:underline}td.diff-deletedline .diffchange{text-decoration:line-through}}`
@@ -156,16 +159,128 @@
 									format: "json",
 									fromrev: searchParams.get("oldid"),
 									torev: searchParams.get("diff"),
+									prop: "diff|ids|parsedcomment|user|timestamp",
 									utf8: 1,
 									formatversion: 2,
 								})
-								.done(d => {
+								.done(async d => {
+									d = d.compare;
+									var next;
+									await api
+										.get({
+											action: "compare",
+											format: "json",
+											fromrev: d.torevid,
+											torelative: "next",
+											prop: "ids",
+											utf8: 1,
+											formatversion: 2,
+										})
+										.done(d => {
+											next = d.compare.torevid;
+										});
+									var prev;
+									await api
+										.get({
+											action: "compare",
+											format: "json",
+											fromrev: d.fromrevid,
+											torelative: "prev",
+											prop: "ids",
+											utf8: 1,
+											formatversion: 2,
+										})
+										.done(d => {
+											prev = d.compare.fromrevid;
+										});
 									document.getElementById(
 										"mw-content-text"
 									).innerHTML = `<table class="diff diff-contentalign-left" data-mw="interface">${
-										d.compare.body
-											? `<colgroup><col class="diff-marker"><col class="diff-content"><col class="diff-marker"><col class="diff-content"></colgroup><tbody>${d.compare.body}`
-											: '<tbody><tr><td colspan="2" class="diff-notice" lang="zh"><div class="mw-diff-empty">（没有差异）</div></td></tr>'
+										d.body
+											? '<colgroup><col class="diff-marker"><col class="diff-content"><col class="diff-marker"><col class="diff-content"></colgroup>'
+											: ""
+									}<tbody><tr class="diff-title" lang="zh"><td colspan="2" class="diff-otitle"><div id="mw-diff-otitle1" class="mw-revslider-older-diff-column"><strong><a href="${mw.util.getUrl(
+										"",
+										{
+											oldid: d.fromrevid,
+										}
+									)}" title="">${parseDate(
+										d.fromtimestamp
+									)}的版本</a> <span class="mw-diff-edit">（<a href="${mw.util.getUrl(
+										"",
+										{
+											action: "edit",
+											oldid: d.fromrevid,
+										}
+									)}" title="${pageName}">编辑</a>）</span></strong></div><div id="mw-diff-otitle2"><a href="${mw.util.getUrl(
+										`User:${d.fromuser}`
+									)}" class="mw-userlink" title="User:${
+										d.fromuser
+									}" data-username="${d.fromuser}"><bdi>${
+										d.fromuser
+									}</bdi></a><span class="mw-usertoollinks">（<a href="${mw.util.getUrl(
+										`User talk:${d.fromuser}`
+									)}" class="mw-usertoollinks-talk" title="User talk:${
+										d.fromuser
+									}">讨论</a> | <a href="${mw.util.getUrl(
+										`Special:Contributions/${d.fromuser}`
+									)}" class="mw-usertoollinks-contribs" title="Special:Contributions/${
+										d.fromuser
+									}">贡献</a>）</span></div><div id="mw-diff-otitle3"><span class="comment">（${
+										d.fromparsedcomment
+									}）</span></div><div id="mw-diff-otitle5"></div><div id="mw-diff-otitle4">${
+										prev
+											? `<a href="${mw.util.getUrl("", {
+													diff: d.fromrevid,
+													oldid: prev,
+											  })}" title="" id="differences-prevlink">←上一编辑</a>`
+											: ""
+									}</div></td><td colspan="2" class="diff-ntitle"><div id="mw-diff-ntitle1" class="mw-revslider-newer-diff-column"><strong><a href="${mw.util.getUrl(
+										"",
+										{
+											oldid: d.torevid,
+										}
+									)}" title="">${parseDate(d.totimestamp)}的${
+										next ? "" : "最新"
+									}版本</a><span class="mw-diff-edit">（<a href="${mw.util.getUrl(
+										"",
+										{
+											action: "edit",
+											oldid: d.torevid,
+										}
+									)}" title="${pageName}">编辑</a>）</span> <span class="mw-diff-undo">（<a href="${mw.util.getUrl(
+										"",
+										{
+											action: "edit",
+											undoafter: d.fromrevid,
+											undo: d.torevid,
+										}
+									)}" title="">撤销</a>）</span></strong></div><div id="mw-diff-ntitle2"><a href="${mw.util.getUrl(
+										`User:${d.touser}`
+									)}" class="mw-userlink" title="" data-username="${
+										d.touser
+									}"><bdi>${
+										d.touser
+									}</bdi></a><span class="mw-usertoollinks">（<a href="${mw.util.getUrl(
+										`User talk:${d.touser}`
+									)}" class="mw-usertoollinks-talk" title="User talk:${
+										d.touser
+									}">讨论</a> | <a href="${mw.util.getUrl(
+										`Special:Contributions/${d.touser}`
+									)}" class="mw-usertoollinks-contribs" title="Special:Contributions/${
+										d.user
+									}">贡献</a>）</span> </div><div id="mw-diff-ntitle3"><span class="comment">（${
+										d.toparsedcomment
+									}）</span></div><div id="mw-diff-ntitle5"></div><div id="mw-diff-ntitle4">${
+										next
+											? `<a href="${mw.util.getUrl("", {
+													diff: next,
+													oldid: d.torevid,
+											  })}" title="${pageName}" id="differences-nextlink">下一编辑→</a>`
+											: ""
+									}</div></td></tr>${
+										d.body ||
+										'<tr><td colspan="2" class="diff-notice" lang="zh"><div class="mw-diff-empty">（没有差异）</div></td></tr>'
 									}</tbody></table>`;
 								});
 						} else {
